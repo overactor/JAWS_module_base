862f76077a46da65e4c31c73c2b347967f472d09
overactor <rik.degraaff@gmx.ch>
2016-03-03 07:19:24 +0100

    initial commit

---
+++ src/jaws/module/base/Handle.java
@@ -0,0 +1,13 @@
+package jaws.module.base;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Handle {
+	String[] extensions();
+	int priority() default 0;
+}
