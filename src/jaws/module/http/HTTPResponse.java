fe2b25ec100747d63f66b2da79c7ad73ad062df7
Roy Geesing <vmuser@192.168.61.128>
2016-04-06 16:17:31 +0200

    Added methods to interface

--- src/jaws/module/http/HTTPResponse.java
+++ src/jaws/module/http/HTTPResponse.java
@@ -2,4 +2,8 @@ package jaws.module.http;
 
 public interface HTTPResponse extends HTTPObject<HTTPResponse> {
 
+	public HTTPResponse statusCode(int i);
+	public int statusCode();
+	public HTTPResponse reason(String string);
+	public String reason();
 }
