61fb9fa0a4ec9c2894ff31ca7c807628366afc38
overactor <rik.degraaff@gmx.ch>
2016-04-11 19:40:59 +0200

    moved javadoc for HTTP objects to interfaces

--- src/jaws/module/http/HTTPRequest.java
+++ src/jaws/module/http/HTTPRequest.java
@@ -1,11 +1,57 @@
 package jaws.module.http;
 
+/**
+ * An interface to represent an HTTP request to the webserver.
+ * 
+ * @author Rik
+ * 
+ * @see jaws.module.http.HTTPResponse
+ */
 public interface HTTPRequest extends HTTPObject<HTTPRequest> {
 
-	HTTPRequest method(RequestMethod valueOf);
+	/**
+	 * Sets the method of the HTTP Request.
+	 * 
+	 * @param method the method of the HTTP Request as an Enum.
+	 * @return the HTTPRequest for method chaining.
+	 */
+	HTTPRequest method(RequestMethod method);
+	
+	/**
+	 * Gets the method of the HTTP Request.
+	 * 
+	 * @return the method of the HTTP Request as an Enum.
+	 */
 	RequestMethod method();
-	HTTPRequest url(String string);
+	
+	
+	/**
+	 * Sets the url of the HTTP Request.
+	 * 
+	 * @param url the url as a String.
+	 * @return the HTTPRequest for method chaining.
+	 */
+	HTTPRequest url(String url);
+	
+	/**
+	 * Gets the url of the HTTP Request.
+	 * 
+	 * @return the url as a String.
+	 */
 	String url();
-	HTTPRequest httpVersion(String string);
+	
+	/**
+	 * Sets the HTTP version of the HTTP Request.
+	 * 
+	 * @param httpVersion the HTTP version as a String.
+	 * @return the HTTPRequest for method chaining.
+	 */
+	HTTPRequest httpVersion(String httpVersion);
+	
+	/**
+	 * Gets the HTTP version of the HTTP Request.
+	 * 
+	 * @return the HTTP version as a String.
+	 */
 	String httpVersion();
 }
--- src/jaws/module/http/HTTPResponse.java
+++ src/jaws/module/http/HTTPResponse.java
@@ -1,9 +1,56 @@
 package jaws.module.http;
 
+/**
+ * An interface to represent an HTTP response from the webserver.
+ * 
+ * @author Rik
+ * 
+ * @see jaws.module.http.HTTPRequest
+ */
 public interface HTTPResponse extends HTTPObject<HTTPResponse> {
 
-	public HTTPResponse statusCode(int i);
+	/**
+	 * Sets the status code of the HTTP response.
+	 * 
+	 * @param code the status code as an integer.
+	 * @return the HTTPResponse for method chaining.
+	 */
+	public HTTPResponse statusCode(int code);
+	
+	/**
+	 * Gets the status code of the HTTP response.
+	 * 
+	 * @return the status code as an integer.
+	 */
 	public int statusCode();
-	public HTTPResponse reason(String string);
+	
+	/**
+	 * Sets the reason of the HTTP response.
+	 * 
+	 * @param reason the reason of the HTTP response.
+	 * @return the HTTPResopnse for method chaining.
+	 */
+	public HTTPResponse reason(String reason);
+	
+	/**
+	 * Gets the reason of the HTTP response. 
+	 * 
+	 * @return the reason as a String.
+	 */
 	public String reason();
+
+	/**
+	 * Gets the HTTP version of the HTTP Response.
+	 * 
+	 * @return the HTTP version as a String.
+	 */
+	public String httpVersion();
+
+	/**
+	 * Sets the HTTP version of the HTTP Response.
+	 * 
+	 * @param httpVersion the HTTP version as a String.
+	 * @return the HTTPResponse for method chaining.
+	 */
+	public HTTPResponse httpVersion(String httpVersion);
 }
