cbe16139b85134e95a6cbc37a22068985d3c5495
Roy Geesing <roy.geesing@hispeed.ch>
2016-04-22 11:01:33 +0200

    added @ContainsHandler annotation for faster module-loading

---
+++ src/jaws/module/net/ContainsHandler.java
@@ -0,0 +1,10 @@
+package jaws.module.net;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface ContainsHandler {}
