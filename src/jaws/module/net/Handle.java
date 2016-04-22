f174bc608c565ebb19c4d5dc745933473d41d625
Roy Geesing <roy.geesing@hispeed.ch>
2016-04-22 14:01:34 +0200

    Handle can take an additional parameter: method (RequestMethod)

--- src/jaws/module/net/Handle.java
+++ src/jaws/module/net/Handle.java
@@ -5,9 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import jaws.module.http.RequestMethod;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface Handle {
 	String[] extensions();
 	int priority() default 0;
+	RequestMethod[] methods() default {RequestMethod.GET, RequestMethod.HEAD, RequestMethod.OPTIONS, RequestMethod.POST};
 }
